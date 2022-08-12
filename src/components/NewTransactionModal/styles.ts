3f13cd1dc18ef0de1d4d40636ce98460fa495a1d
rafabrazil <rafael.h@outlook.com>
2022-08-12 17:34:06 -0300

    Estrutura visual - Modal de nova transação

---
+++ src/components/NewTransactionModal/styles.ts
@@ -0,0 +1,72 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import styled from 'styled-components'
+
+export const Overlay = styled(Dialog.Overlay)`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  inset: 0;
+  background-color: rgba(0, 0, 0, 0.75);
+`
+
+export const Content = styled(Dialog.Content)`
+  min-width: 32rem;
+  border-radius: 6px;
+  padding: 2.5rem 3rem;
+  background-color: ${(props) => props.theme['gray-800']};
+
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  form {
+    margin-top: 2rem;
+
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+    input {
+      border-radius: 6px;
+      border: 0;
+      background-color: ${(props) => props.theme['gray-900']};
+      color: ${(props) => props.theme['gray-300']};
+      padding: 1rem;
+
+      &::placeholder {
+        color: ${(props) => props.theme['gray-500']};
+      }
+    }
+
+    button[type='submit'] {
+      height: 58px;
+      border: 0;
+      background-color: ${(props) => props.theme['green-500']};
+      border-radius: 6px;
+      padding: 0 1.25rem;
+      color: ${(props) => props.theme.white};
+      font-weight: bold;
+      margin-top: 1.5rem;
+      cursor: pointer;
+
+      &:hover {
+        background-color: ${(props) => props.theme['green-700']};
+        transition-property: color, background-color, border-color,
+          text-decoration-color, fill, stroke;
+        transition-duration: 0.2s;
+      }
+    }
+  }
+`
+
+export const CloseButton = styled(Dialog.Close)`
+  position: absolute;
+  border: 0;
+  background: transparent;
+  top: 1.5rem;
+  right: 1.5rem;
+  line-height: 0;
+  cursor: pointer;
+  color: ${(props) => props.theme['gray-500']};
+`
